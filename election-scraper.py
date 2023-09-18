b6312a151fe6a3bf677c233d2af0e563c25484b1
Jan Slechta <janslechta31@gmail.com>
2023-09-18 16:01:38 +0200

    Added comments for function, arguments etc.

--- election-scraper.py
+++ election-scraper.py
@@ -10,16 +10,23 @@ from requests import get
 from bs4 import BeautifulSoup 
 import sys
 
+"""
+    Retrieves the content of a web page based on the provided URL using an HTTP GET request and parses it with BeautifulSoup.
 
-#if len(sys/argv) != 3:
-#        print("zadej dva argumenty")
-
-
-
+    :param url: URL address of the web page to analyze.
+    :return: BeautifulSoup object representing the analyzed page.
+"""
 def get_page(url):
     response = get(url)
     return BeautifulSoup(response.text, features="html.parser") 
 
+
+"""
+    Scrapes information about election results from a web page and stores it in a dictionary.
+
+    :param url: URL address of the web page with election results.
+    :return: Dictionary containing extracted data about election results.
+"""
 def scrape_web_page(url):
     soup = get_page(url)
     extracted_data = {}
@@ -31,14 +38,11 @@ def scrape_web_page(url):
             url = td.find('a', href= True)
             data_url_location = "https://volby.cz/pls/ps2017nss/"+url['href']
             location = tr.find('td', class_="overflow_name").string
-
-
             soup2 = get_page(data_url_location)
             table_data2 = soup2.find_all("td", class_ ='cislo')
             registred = table_data2[3].string
             envelopes = table_data2[4].string
             valid = table_data2[7].string
-
             extracted_data[code] = {
                 'Kód obce' : code,
                 'Název obce': location,
@@ -72,7 +76,12 @@ def scrape_web_page(url):
         
     return extracted_data
 
+"""
+    Retrieves the header for a CSV file based on a web page with election results.
 
+    :param url: URL address of the web page with election results.
+    :return: List containing the header for the CSV file.
+"""
 def get_header(url):
     soup = get_page(url)
     try:
@@ -84,7 +93,7 @@ def get_header(url):
     except AttributeError:
         print("Nastala chyba při stahování nebo řazení dat,zkontrolujte správnost adresy pro stažení. Ukončuji program.")
         quit()
-    
+            
     for i in political_parties:
         try:
             party = i.find('td', class_='overflow_name').string
@@ -93,8 +102,14 @@ def get_header(url):
             continue
     return list(header)
 
+"""
+    Writes extracted data to a CSV file with consideration of a predefined header.
 
-def write_to_csv(extracted_data,output_file):
+    :param extracted_data: Extracted data about election results.
+    :param output_file: Name of the output CSV file.
+    :param url: URL address of the web page with election results.
+"""
+def write_to_csv(extracted_data,output_file,url):
     with open(output_file, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         header = get_header(url)
@@ -103,21 +118,41 @@ def write_to_csv(extracted_data,output_file):
             obec_data_filtered = {key: location_data[key] for key in header}
             writer.writerow(obec_data_filtered.values())
 
-#def main(url,file):
-#    try:
-#        print(f"Data processing")
-#    except AttributeError:
-#        print("Unexpected error")
-#        quit()
+
+"""
+    Checks the validity of command-line arguments when running the program.
+
+    :param argv: List of arguments passed to the program.
+"""
+def check_arguments(argv):
+    if len(argv) != 3:
+        print(f"Invalid number of arguments,need to be 2 arguments,inserted {(len(argv) - 1)}")
+        quit()
+    elif "https://volby.cz/pls/ps2017nss/" not in argv[1]:
+        print("Invalid URL for download data")
+        quit()
+    elif not argv[2].endswith(".csv"):
+        print("Invalid syntax of output file, name of the file must have a .csv extension")
+        quit()
+
+"""
+    Main function of the program for scraping election results and saving them to a CSV file.
+
+    :param url: URL address of the web page with election results.
+    :param output_file: Name of the output CSV file.
+"""
+def main(url,output_file):
+    try:
+        print(f"Data processing")
+    except AttributeError:
+        print("Unexpected error, ending the program")
+        quit()
     
-#    extracted_data = scrape_web_page(url)
-#    print(f"Resuls saved as: '{file}'")
-#    output_file = file + '.csv'
-#    write_to_csv(extracted_data,output_file)
+    extracted_data = scrape_web_page(url)
+    print(f"Results saved as: '{output_file}'")
+    write_to_csv(extracted_data,output_file,url)
+    print("quiting election-scraper")
 
 if __name__ == '__main__':
-    url = 'https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103'
-    extracted_data = scrape_web_page(url)
-    output_file = 'vysledky.csv'
-    write_to_csv(extracted_data,output_file)
-    
+    check_arguments(sys.argv)
+    main(sys.argv[1], sys.argv[2])
